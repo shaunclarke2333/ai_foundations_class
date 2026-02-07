2d9837947723b897298b062f70d81a28a16c7442
Shaun Clarke <shaunclarke43@gmail.com>
2026-02-07 22:13:25 +0000

    updated readne

--- week4_knowledge_language_generation/shaun_clarke_csc6313/shaun_clarke_csc6313_rag_chat_bot.py
+++ week4_knowledge_language_generation/shaun_clarke_csc6313/shaun_clarke_csc6313_rag_chat_bot.py
@@ -20,8 +20,11 @@ import requests
 import pandas as pd
 import pymupdf4llm
 
-
+# This class loads documents to be chunked
 class DocumentLoader:
+    """
+    This class loads documents, parses the content and stores them as chunks in a list of dictionaries.
+    """
 
     def __init__(self, chunk_size: int = 500, chunk_overlap: int = 50):
         """
@@ -45,12 +48,12 @@ class DocumentLoader:
             "row": row
         }
         return file_data
-    
+
     # This method returns content from a text file or markdown file
     def get_text_content(self, file_path: str) -> str:
         """
         Docstring for get_text_content
-        
+
         :param self: Description
         :param file_path: Description
         :type file_path: str
@@ -65,15 +68,15 @@ class DocumentLoader:
                 # Returning file content
                 return text_file_content
         except FileNotFoundError:
-            raise(f"{file_path} could not be found")
+            raise (f"{file_path} could not be found")
         except Exception as e:
-            raise(f"Seems like we hvave a problem {e}")
-    
-    # This method returns content from a pdf file 
+            raise (f"Seems like we hvave a problem {e}")
+
+    # This method returns content from a pdf file
     def get_pdf_content(self, file_path: str, documents: List, collect_content_func: Callable[[str, str], Dict]) -> None:
         """
         This method gets the contents of a pdf file along with the page num and file name
-        
+
         :param self: Description
         :param file_path: The location of the file
         :type file_path: str
@@ -84,28 +87,33 @@ class DocumentLoader:
         """
         try:
             # Getting PDF metadata that includes content, page number and file path. page_chunks=True makes the metadata available
-            pages: List[Dict[str, str]] = pymupdf4llm.to_markdown(file_path, page_chunks=True)
-            #Looping through the list fo dictionaries to extract the needed info from the PDF metadata
+            pages: List[Dict[str, str]] = pymupdf4llm.to_markdown(
+                file_path, page_chunks=True)
+            # Looping through the list fo dictionaries to extract the needed info from the PDF metadata
             for page_dict in pages:
-                page_content: str = page_dict["text"] # Extracting the pdf page content
-                metadata: str = page_dict["metadata"] # Getting metadata for the specific page so we can get the page number later
-                page_num: int = metadata["page"] # Extracting number for the specific page
+                # Extracting the pdf page content
+                page_content: str = page_dict["text"]
+                # Getting metadata for the specific page so we can get the page number later
+                metadata: str = page_dict["metadata"]
+                # Extracting number for the specific page
+                page_num: int = metadata["page"]
                 # filename: str = os.path.basename(metadata["file_path"]) # Stripping path to get only filename
 
                 # creating a dictionary with the needed page details
-                pdf_data_dict = collect_content_func(file_path, page_content, page_num)
+                pdf_data_dict = collect_content_func(
+                    file_path, page_content, page_num)
                 # adding page data  to documents list
                 documents.append(pdf_data_dict)
         except FileNotFoundError:
-            raise(f"{file_path} could not be found")
+            raise (f"{file_path} could not be found")
         except Exception as e:
-            raise(f"Seems like we have a problem {e}")
-        
+            raise (f"Seems like we have a problem {e}")
+
     # This method returns the content from a CSV
     def get_csv_content(self, file_path: str, documents: List, collect_content_func: Callable[[str, str], Dict]) -> None:
         """
         This method gets the contents of a csv file along with the row num and file name
-        
+
         :param self: Description
         :param file_path: The location of the file
         :type file_path: str
@@ -119,35 +127,36 @@ class DocumentLoader:
             df = pd.read_csv(file_path)
             # Looping through the dataframe getting the index and row data
             # the index is the row number
-            # # the row is a pandas series that has the column value parirs for that row 
+            # # the row is a pandas series that has the column value parirs for that row
             for index, row in df.iterrows():
                 # Empty list to hold the formatted column: value strings the specified row
-                lines: list =[]
+                lines: list = []
                 # replacing all missing NaN values in the specifed row with Unknown
                 row = row.fillna('Unknown')
                 # looping through each column value pair in the specified row
-                # column is the column name 
+                # column is the column name
                 # value is going to be the value for that column in the specified row
                 for column, value in row.items():
-                    # adding column and value as key: value pairs so the row data makes sense 
+                    # adding column and value as key: value pairs so the row data makes sense
                     lines.append(f"{column}: {value}")
                 # Joining all the column: value lines as one multi line string
                 content = "\n".join(lines)
 
-                # Creating a dictionary with the specified row's data. 
-                csv_data_dict = collect_content_func(file_path, content, None, index)
+                # Creating a dictionary with the specified row's data.
+                csv_data_dict = collect_content_func(
+                    file_path, content, None, index)
                 # Adding the processed row to the documents list
                 documents.append(csv_data_dict)
         except FileNotFoundError:
-            raise(f"{file_path} could not be found")
+            raise (f"{file_path} could not be found")
         except Exception as e:
-            raise(f"Seems like we have a problem {e}")
+            raise (f"Seems like we have a problem {e}")
 
     # This method reads all files from a specified directory
     def load_documents(self, directory: str) -> List[Dict[str, str]]:
         """
         Docstring for load_documents
-        
+
         :param self: Description
         :param directory: Description
         :type directory: str
@@ -168,13 +177,13 @@ class DocumentLoader:
             # Creating absolute filepath
             file_path: str = os.path.join(directory, file)
             # Getting file extension by using split, also using a throw away variable for the filename becasue we only need the extension
-            _: str 
+            _: str
             extension: str
             _, extension = os.path.splitext(file)
             # making sure extension is lower case
             extension: str = extension.lower()
 
-             # If this item is not a file skip it.
+            # If this item is not a file skip it.
             if not os.path.isfile(file_path):
                 # print(f"This is a directory, and here is the path: {file_path}")
                 continue
@@ -182,40 +191,44 @@ class DocumentLoader:
             if extension not in doc_types:
                 # print(f"This extension cannot be parsed at the moment: {extension}")
                 continue
-            
+
             # If this is a .txt file parse it as a text file
             if extension == ".txt":
-               # Getting content from text file
-               text_file_content: str = self.get_text_content(file_path)
-               # Using a heler function to create a dictionary with the file name and content before adding it to the documents list
-               text_data_dict = self.collect_file_content(source=file_path, content=text_file_content)
-               # Adding text file content to list
-               documents.append(text_data_dict)
+                # Getting content from text file
+                text_file_content: str = self.get_text_content(file_path)
+                # Using a heler function to create a dictionary with the file name and content before adding it to the documents list
+                text_data_dict = self.collect_file_content(
+                    source=file_path, content=text_file_content)
+                # Adding text file content to list
+                documents.append(text_data_dict)
 
             # If this is a PDF file parse it as a PDF file
             if extension == ".pdf":
                 # Getting content and metadata from pdf file
-                self.get_pdf_content(file_path, documents, self.collect_file_content)
+                self.get_pdf_content(file_path, documents,
+                                     self.collect_file_content)
 
             # If this is a markdown file .md parse it as such
             if extension == ".md":
                 # Getting content from text file using get_text_content function because it works for .md files as well
                 markdown_file_content: str = self.get_text_content(file_path)
                 # Using a heler function to create a dictionary with the file name and content before adding it to the documents list
-                markdown_data_dict = self.collect_file_content(source=file_path, content=markdown_file_content)
+                markdown_data_dict = self.collect_file_content(
+                    source=file_path, content=markdown_file_content)
                 # Adding markdown file content to list
                 documents.append(markdown_data_dict)
-            
+
             # If this is a CSV file parse it as a .CSV
             if extension == ".csv":
                 # Getting content, row number and filename from the csv
-                self.get_csv_content(file_path, documents, self.collect_file_content)
-                
+                self.get_csv_content(file_path, documents,
+                                     self.collect_file_content)
+
         return documents
-    
-    # 
-    def chunk_text(self, text: str, source: str, 
-               page: Optional[int] = None, row: Optional[int] = None) -> List[Dict[str, str]]:
+
+    #
+    def chunk_text(self, text: str, source: str,
+                   page: Optional[int] = None, row: Optional[int] = None) -> List[Dict[str, str]]:
         """
         Split text into overlapping chunks while preserving metadata.
 
@@ -241,11 +254,12 @@ class DocumentLoader:
         # If the amount of text is smaller than the chunk_size, return a single cunk
         if len(text) <= chunk_size:
             # Creating dictionary of chunked text
-            chunk_dict: Dict = self.collect_file_content(source, text, page, row)
+            chunk_dict: Dict = self.collect_file_content(
+                source, text, page, row)
             # adding chunked text dictionary to chunks list
             chunks.append(chunk_dict)
             return chunks
-        
+
         # the step size determines how far the chunking window/range advances through the text
         # Step_size = chunk_size - chunk_overlap, this means the chunking window will always be slightly smaller than the chunk_size
         # So step_size is what enforces the overlap
@@ -259,12 +273,23 @@ class DocumentLoader:
             # Chunking text by using start and end to slice the text
             chunk_text: str = text[starting_slice_position:ending_slice_position]
             # Creating dictionary of chunked text
-            chunk_dict: Dict = self.collect_file_content(source, chunk_text, page, row)
+            chunk_dict: Dict = self.collect_file_content(
+                source, chunk_text, page, row)
             # Adding dictionary of chunked text to list
             chunks.append(chunk_dict)
             # If we are at the end of the text then break the loop
             if ending_slice_position >= len(text):
                 break
-            
-        return chunks
 
+        return chunks
+    
+# This class manages Chroma DB and stores chunks as embeddings
+class VectorStore:
+    """
+    This class:
+    - initializes ChromaDB and the embeddingmodel.
+    - Stores chunked text as vector embeddings.
+    - Handles semantic similarity searches
+    """
+    def __init__(self):
+        pass
