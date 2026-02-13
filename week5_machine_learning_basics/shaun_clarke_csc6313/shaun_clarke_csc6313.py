682672ada3d7d9568f41298c3c9de5b0606111cc
Shaun Clarke <shaunclarke43@gmail.com>
2026-02-13 23:09:45 +0000

    started week 5 project

--- week5_machine_learning_basics/shaun_clarke_csc6313/shaun_clarke_csc6313.py
+++ week5_machine_learning_basics/shaun_clarke_csc6313/shaun_clarke_csc6313.py
@@ -3,7 +3,7 @@ Name: Shaun Clarke
 Course: CSC6313 Ai Foundations
 Instructor: Margaret Mulhall
 Module: 5
-Assignment: The Knowledge-Grounded Assistant (RAG)
+Assignment: The Diagnostic Prediction Engine
 
 In this project, you will develop a supervised machine learning pipeline to predict patient health outcomes using Scikit-Learn.
 You will transition from "Classical ML" theory to a functional Python implementation that handles data generation, cleaning, model training, and real-time inference.
